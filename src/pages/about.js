8a0555dd9f02c1101b557b69f97ad7efa54f949b
Peter Kruzlics <peterk24@gmail.com>
2020-01-05 16:33:34 -0500

    Rename component

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
-class BlogIndex extends React.Component {
+class About extends React.Component {
   render() {
     const { data } = this.props
 
@@ -22,7 +22,7 @@ class BlogIndex extends React.Component {
   }
 }
 
-export default BlogIndex
+export default About
 
 export const pageQuery = graphql`
   query {
